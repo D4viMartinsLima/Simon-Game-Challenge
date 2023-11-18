a2d37e68565f190cc9753f581fc83ed62d8c1244
Davi Martins <martinslimafkc@gmail.com>
2023-11-18 15:29:45 -0300

    Checking which button is pressed

--- game.js
+++ game.js
@@ -1,10 +1,26 @@
 'use strict'
 let gamePattern = [];
 
+let userClickedPattern = [];
+
 let buttonColours = ["red", "blue", "yellow", "green"];
 
 function nextSequence(){
     let randomNumber = Math.floor(Math.random() * 4);
-    let randomChosenColour =  buttonColours[randomNumber];
+    let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    // let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    // audio.play();
+
+
+    $("#btn").on("click", function(){
+        let userChosenColour = $(this).attr("id");
+        userClickedPattern.push(userChosenColour);
+        console.log(userClickedPattern)
+    })
 }
+
+nextSequence();
